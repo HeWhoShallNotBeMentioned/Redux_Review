cf0cba576cc085da210f27e34ac4837e91dc9e9c
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-30 13:54:33 -0700

    connect redux to react component with Providor and Connect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import rootReducer from './Store/reducer';
+import reducer from './Store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore();
+const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
